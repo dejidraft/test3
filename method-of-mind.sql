abcefece78d0236b4acfd9dbd1b856033081673e
dejidraft <navarrodejose@gmail.com>
2025-12-28 16:35:21 -0800

    Update method-of-mind.sql

--- method-of-mind.sql
+++ method-of-mind.sql
@@ -1,10 +1,10 @@
-method-of-mind/
-│
-├── index.html
-├── about.html
-├── topics.html
-├── research-methods.html
-├── biopsych.html
-├── personality.html
-├── styles.css
+/method-of-mind/
+index.html
+about.html
+topics.html
+research-methods.html
+biopsych.html
+personality.html
+styles.css
+
 
